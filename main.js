9ebaa5d24de8d89bcf91f5a0f25ef66ca2e13afa
moazsoliman294 <moazsoliman66@gmail.com>
2025-06-02 16:03:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,310 @@
+const menu=[
+ {
+    id:1,
+    title:"Buttermilk pancakes",
+    category:"breakfast",
+    price:50 + " L.E",
+    img:"download (6).jfif",
+    desc:`made by culturing ordinary skim milk with lactic acid bacteria.`,
+    rating:"★★★☆☆"
+ },
+ {
+    id:2,
+    title:"Burger",
+    category:"lunch",
+    price:185 + " L.E",
+    img:"download (7).jfif",
+    desc:`Grilled ground beef placed between two halves of bread, with lettuce slices, tomatoes and various sauces. `,
+     rating:"★★★★☆"
+ },
+ {
+    id:3,
+    title:"godzilla milkshake",
+    category:"drinks",
+    price:70 + " L.E",
+    img:"download (8).jfif",
+    desc:`Milo, condensed milk, hot water, ice, milk, whipped cream, and chocolate syrup`,
+    rating:"★★☆☆☆"
+
+ },
+ {
+    id:4,
+    title:"sandwichs",
+    category:"breakfast",
+    price:35 + " L.E",
+    img:"images.jfif",
+    desc:`The sandwiches consist of eggs,pastirma slices, and vegetables.`,
+    rating:"★★★☆☆"
+ },
+ {
+    id:5,
+    title:"Pizza",
+    category:"lunch",
+    price:90 + " L.E",
+    img:"download (9).jfif",
+    desc:`leavened wheat-based dough topped with tomato, cheese, and other ingredients, baked at a high temperature.`,
+    rating:"★★★★☆"
+ },
+ {
+    id:6,
+    title:"Chicken soup",
+    category:"soup",
+    price:50 + " L.E",
+    img:"download (10).jfif",
+    desc:`chicken, simmered in water, usually with various other ingredients.`,
+    rating:"★★☆☆☆"
+ },
+ {
+    id:7,
+    title:"Shawarma",
+    category:"lunch",
+    price:65 + " L.E",
+    img:"download (11).jfif",
+    desc:`thin cuts of seasoned and marinated lamb, mutton, veal, beef, chicken, or turkey.`,
+    rating:"★★★★★"
+ },
+ {
+    id:8,
+    title:"Hot Dog",
+    category:"lunch",
+    price:55 + " L.E",
+    img:"images (1).jfif",
+    desc:`a dish consisting of a grilled, steamed, or boiled sausage served in the slit of a partially sliced bun.`,
+    rating:"★★★☆☆"
+ },
+ {
+    id:9,
+    title:"Ice cream",
+    category:"dessart",
+    price:20 + " L.E",
+    img:"download (12).jfif",
+    desc:`Elegant chocolate chips, colorful candy pieces, and crunchy waffle cones topped with ice cream.`,
+    rating:"★☆☆☆☆"
+ },
+ {
+    id:10,
+    title:"Donut",
+    category:"dessart",
+    price:45 + " L.E",
+    img:"download.jfif",
+    desc:`a kind of ring-shaped snack food popular in many countries, which are usually deep fried from flour doughs.`,
+    rating:"★★★☆☆"
+ },
+ {
+    id:11,
+    title:"Cookie",
+    category:"dessart",
+    price:40 + " L.E",
+    img:"download (14).jfif",
+    desc:`They are small, sweet, and varied cakes, cut from rolled dough, cut into pieces after baking, or rolled with a special iron.`,
+    rating:"★★☆☆☆"
+ },
+ {
+    id:12,
+    title:"Tart",
+    category:"dessart",
+    price:120 + " L.E",
+    img:"download (15).jfif",
+    desc:`a baked dish consisting of a filling over a pastry base with an open top not covered with pastry.`,
+    rating:"★★★★☆"
+ },
+ {
+    id:13,
+    title:"vegetable soup",
+    category:"soup",
+    price:25 + " L.E",
+    img:"download (17).jfif",
+    desc:` Vegetable soup consists of baby carrots, potatoes, onions, celery, tomatoes, green beans, chicken broth, vegetable broth, water, basil, thyme, and salt.`,
+    rating:"★★☆☆☆"
+ },
+ {
+    id:14,
+    title:"Grilled fish",
+    category:"lunch",
+    price:90 + " L.E",
+    img:"download (18).jfif",
+    desc:`A piece of fish cooked on the grill and is very delicious with lemon.`,
+    rating:"★★★☆☆"
+ },
+ {
+    id:15,
+    title:"Sushi",
+    category:"lunch",
+    price:115 + " L.E",
+    img:"download (19).jfif",
+    desc:` consisting of cooked rice flavoured with vinegar and a variety of vegetable, egg, or raw seafood garnishes and served cold.`,
+    rating:"★★★★☆"
+ },
+ {
+    id:16,
+    title:"spaghetti",
+    category:"lunch",
+    price:45 + " L.E",
+    img:"download (20).jfif",
+    desc:`A food made primarily from a mixture of flour and water dried into thin, hard strands with tomato sauce.`,
+    rating:"★★★☆☆"
+ },
+ {
+    id:17,
+    title:"Eggs",
+    category:"breakfast",
+    price:25 + " L.E",
+    img:"img/images.jfif",
+    desc:`The dish consists of an egg cooked in oil or fat with slices of pastirma and bread.`,
+    rating:"★★☆☆☆"
+ },
+ {
+    id:18,
+    title:"chicken",
+    category:"lunch",
+    price:110 + " L.E",
+    img:"download (5).jfif",
+    desc:`chicken parts or entire chickens that are barbecued, grilled or smoked.`,
+    rating:"★★★★☆"
+ },
+ {
+    id:19,
+    title:"Macaroni Bechamel",
+    category:"lunch",
+    price:90 + " L.E",
+    img:"download (1).jfif",
+    desc:`Macaroni and cheese is made with butter, flour, milk, ground beef or chicken, and mozzarella cheese.`,
+    rating:"★★★☆☆"
+ },
+ {
+    id:20,
+    title:"Lentil soup",
+    category:"soup",
+    price:50 + " L.E",
+    img:"download (2).jfif",
+    desc:`Lentil soup is made of lentils, onions, garlic and spices and is very delicious with lemon.`,
+    rating:"★★☆☆☆"
+ },
+ {
+    id:21,
+    title:"Cupcake",
+    category:"dessart",
+    price:60 + " L.E",
+    img:"download (3).jfif",
+    desc:`Cupcakes are made of flour, eggs, sugar, and vanilla and are decorated with chocolate and colorful candy.`,
+    rating:"★★★★☆"
+ },
+ {
+    id:22,
+    title:"Lemon juice",
+    category:"drinks",
+    price:20 + " L.E",
+    img:"download (4).jfif",
+    desc:`Lemonade is made from water, sugar, lemon, and ice, and Green mint`,
+    rating:"★★☆☆☆"
+ },
+ {
+    id:23,
+    title:"Orange juice",
+    category:"drinks",
+    price:15 + " L.E",
+    img:"download (21).jfif",
+    desc:`Orange juice is made from fresh, natural oranges, sugar, orange juice, and ice.`,
+    rating:"★★☆☆☆"
+ },
+  {
+    id:24,
+    title:"Strawberry juice",
+    category:"drinks",
+    price:30 + " L.E",
+    img:"image.png",
+    desc:`Made with fresh strawberries, sugar, and a little water, it will leave you feeling refreshed.`,
+    rating:"★★★☆☆"
+ },
+];
+
+//DOMContentLoaded .
+
+const sectioncenter=document.querySelector(".card-ss");
+const btncontainer=document.querySelector(".btn-container");
+
+window.addEventListener("DOMContentLoaded",function(){
+    displaymenuitem(menu);
+    displaymenubtn();
+});
+
+ //map that displays all items of the menu
+
+function displaymenuitem(menuitem){
+
+    let displaymenu = menuitem.map(function (item) {
+
+        return`
+         <div class="cards">
+    <div class="card">
+    <img class="moaz" src="${item.img}" alt="${item.title}">
+    <p id="ww">${item.desc}</p>
+      
+      <h1>${item.price}</h1>
+    <div class="card2"></div>
+    <h5 id="hh">${item.title}</h5>
+</div>
+</div>
+        `;
+    });
+
+    displaymenu=displaymenu.join("");
+    //inner HTML added
+    sectioncenter.innerHTML=displaymenu;
+}
+ //Reduce used to filter the items in the menu with respect to their categories
+
+function displaymenubtn(){
+    const categories=menu.reduce(
+        function (values,item) {
+            if(!values.includes(item.category)){
+                values.push(item.category);
+            }
+            return values;
+        },
+        ["all"]
+    );
+
+
+    const categorybtns=categories
+    .map(function (category) {
+         return `
+         <div class="fillter"> <a href="#card-ss"><button  type="button" class="filter-btn" data-id=${category}>
+            ${category}
+          </button> </a>
+          </div>`;
+    })
+    .join("");
+
+    btncontainer.innerHTML=categorybtns;
+
+    const filterbtns=btncontainer.querySelectorAll(".filter-btn");
+    console.log(filterbtns);
+ 
+         filterbtns.forEach(function (btn) {
+         btn.addEventListener("click", function (e) {
+            const category= e.currentTarget.dataset.id;
+            const menucategory= menu.filter(function (menuitem) {
+                if (menuitem.category===category) {
+                    return menuitem;
+                }
+            });
+            if (category==="all") {
+                displaymenuitem(menu);
+            }else{
+                displaymenuitem(menucategory);
+            }
+        });
+    });
+};
+
+const items=document.querySelectorAll('.card');
+
+window.addEventListener('scroll',()=>{
+   items.forEach((item)=>{
+      const rect = item.getBoundingClientRect();
+      if (rect.top<window.innerHeight) {
+         item.classList.add('show');
+      }
+   });
+});
